6b086c4437b72d6f4507d4f3339fdeb5a3e30070
goodakai <129071425+goodakai@users.noreply.github.com>
2024-03-27 20:38:07 +0800

    Update GEE_mapping.js

--- GEE_mapping.js
+++ GEE_mapping.js
@@ -3,37 +3,38 @@
 // contact: Ziye Wang (Email: ziyewang@cug.edu.cn)
 
 // input vector file
-var aoi = ee.FeatureCollection("projects/ee-kaifangguo/assets/AOI");
-var hl_aoi = ee.FeatureCollection("projects/ee-kaifangguo/assets/cndhl");
-var unhl_aoi = ee.FeatureCollection("projects/ee-kaifangguo/assets/unhl");
+var aoi = ee.FeatureCollection("Asset ID/Asset name1");// uploaded vector file of the Himalayan orogenic belt region
+var hl_aoi = ee.FeatureCollection("Asset ID/Asset name2");// uploaded vector file of the Cuonadong dome region
+var unhl_aoi = ee.FeatureCollection("Asset ID/Asset name3");// uploaded vector file of the Himalayan orogenic belt area excluding the area of Cuonadong dome
 
 // Sentinel2 cloud mask function
 function maskS2clouds(image)
 {
-        var  qa = image.select('QA60');
-        var  cloudBitMask = 1 << 10;
-        var  cirrusBitMask = 1 << 11;
+        var  qa = image.select('QA60');// cloud mask band
+        var  cloudBitMask = 1 << 10;// the 10th digit of QA60 represents the probability of cloud presence
+        var  cirrusBitMask = 1 << 11;// the 11th digit of QA60 represents the probability of cirrus presence
         var mask = qa.bitwiseAnd(cloudBitMask).eq(0)
                      .and(qa.bitwiseAnd(cirrusBitMask).eq(0));
-return image.updateMask(mask).divide(100);
+return image.updateMask(mask).divide(100);// for consistency with the ASTER value range divided by 100
 }
 
 // input ASTER and Sentinel2 image
-var asterimage =  ee.Image("projects/ee-kaifangguo/assets/asterjz")
+var asterimage =  ee.Image('Asset ID/Asset name3')// uploaded TIF file of atmospherically corrected ASTER image
                     .select(['green','red','nir','swir1','swir2','swir3','swir4','swir5','swir6'],
                             ['A01','A02','A3N','A04','A05','A06','A07','A08','A09'])
-                    .multiply (100)
+                    .multiply (100)// for consistency with the Sentinel2 value range multiply by 100
                     .clip(aoi);
                     
-var sentinel2image =  ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
-                   .filterDate('2020-01-01','2022-12-31')
-                   .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',10))
+var sentinel2image =  ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")// calling Sentinel-2 image data from GEE cloud servers
+                   .filterDate('2020-01-01','2022-12-31')// filtering the date range in which the image was taken
+                   .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',10))// filtering images with less than 10% cloud parameter
                    .filterBounds(aoi)
                    .map(maskS2clouds)
                    .select(['B2','B3','B4','B5','B6','B7','B8','B8A','B11','B12'],
                            ['S02','S03','S04','S05','S06','S07','S08','S8A','S11','S12'])
                    .median()
-                   .clip(aoi);
+                   .clip(aoi)
+                   .reproject('EPSG:4326',null,100);// resampling of sentinel2 data to EPSG:4326 and 100 m resolution
 
 // select remote sensing bands
 var asterbands = asterimage.select('A3N','A04','A06','A07');
@@ -43,17 +44,17 @@ print(allimage);
 var bands = allimage.bandNames();
 
 // production of train data
-var positive_po = ee.FeatureCollection.randomPoints(hl_aoi,4000);
-var negative_po = ee.FeatureCollection.randomPoints(unhl_aoi,4000);
+var positive_po = ee.FeatureCollection.randomPoints(hl_aoi,4000);// 4000 randomly selected pixels in the leucogranite area of Cuonadng
+var negative_po = ee.FeatureCollection.randomPoints(unhl_aoi,4000);// 4000 randomly selected pixels in unknown area
 
 function add_1(feature)
 {
-  return feature.set('Lithology', 1);
+  return feature.set('Lithology', 1);// add label 1 to positive data
 }
 
 function add_0(feature)
 {
-  return feature.set('Lithology', 0);
+  return feature.set('Lithology', 0);// add label 0 to negative data
 }
 
 var positive_data = positive_po.map(add_1);
@@ -63,13 +64,13 @@ var train_points = positive_data.merge(negative_data);
 
 var train_data = allimage.sampleRegions({
   collection: train_points,
-  properties: ['Lithology'],
-  scale: 100
+  properties: ['Lithology'],// copy label values to the property list
+  scale: 100// spatial resolution for extracting pixel values
 });
 
 // training the random forest model
 var withRandom = train_data.randomColumn('random');
-var split = 0.7; 
+var split = 0.7; // set a threshold for splitting the dataset
 var trainingPartition = withRandom.filter(ee.Filter.lt('random', split));
 var testingPartition = withRandom.filter(ee.Filter.gte('random', split));
 
@@ -80,7 +81,7 @@ var rfModel = ee.Classifier.smileRandomForest(15).setOutputMode('PROBABILITY').t
   classProperty: 'Lithology',
   inputProperties: bands
   }
-);
+);//construct a rf model with 15 trees and set it to PROBABILITY output mode
 
 // test model performance
 var validated = testingPartition.classify(rfModel);
@@ -88,12 +89,12 @@ var validated = testingPartition.classify(rfModel);
 var probToClass = function(feature) 
 {
   var prob = ee.Number(feature.get('classification'));
-  var label = prob.gte(0.5).toInt();
+  var label = prob.gte(0.5).toInt();// the results of the testingPartition were split according to a threshold of 0.5
   return feature.set('predicted', label);
 };
 
 var validatedClass = validated.select('Lithology','classification').map(probToClass);
-var confusionMatrix = validatedClass.errorMatrix('Lithology', 'predicted');
+var confusionMatrix = validatedClass.errorMatrix('Lithology', 'predicted');// calculate the confusionMatrix
 print('confusionMatrix',confusionMatrix);
 print('overall accuracy', confusionMatrix.accuracy());
 print('kappa accuracy', confusionMatrix.kappa());
@@ -104,19 +105,19 @@ print(img_classfication);
 var exportParams = 
 {
   image: img_classfication,
-  description: '5bands',
-  folder: 'GEE',
-  fileNamePrefix: '5bands',
+  description: 'description',//exporting task descriptions
+  folder: 'folder',//export to Google Cloud Drive folder
+  fileNamePrefix: 'fileName',// name of the exported file
   region: aoi,
-  scale: 100,
-  crs: 'EPSG:4326',
+  scale: 100,// resolution of the exported file
+  crs: 'EPSG:4326',// coordinate of the exported file
   fileFormat: 'GeoTIFF'
 };
 Export.image.toDrive(exportParams);
 
 // visualization
-Map.addLayer(asterimage, {min:0, max:100, bands:['A3N', 'A02','A01']}, 'ASTER');
-Map.addLayer(sentinel2image, {min:0, max:80, bands:['S04', 'S03','S02']}, 'Sentinel2');
-Map.addLayer(img_classfication, {min:0, max:1, palette:['yellow', 'red','blue']}, 'img_classfication');
-Map.addLayer(aoi, {}, 'aoi');
-Map.centerObject(aoi);
+Map.addLayer(asterimage, {min:0, max:100, bands:['A3N', 'A02','A01']}, 'ASTER');// show ASTER image
+Map.addLayer(sentinel2image, {min:0, max:80, bands:['S04', 'S03','S02']}, 'Sentinel2');// show Sentinel2 image
+Map.addLayer(img_classfication, {min:0, max:1, palette:['yellow', 'red','blue']}, 'img_classfication');// show leucogranite mapping image
+Map.addLayer(aoi, {}, 'aoi');// show study area boundaries
+Map.centerObject(aoi);// using the study area as a display center
