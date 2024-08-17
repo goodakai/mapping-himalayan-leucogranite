9e19635e374c66af552138e9488fc6447811c57b
goodakai <129071425+goodakai@users.noreply.github.com>
2024-08-17 15:37:11 +0800

    Update Calling U-net model for lithology mapping in GEE.js

--- U-Net FCNN/Calling U-net model for lithology mapping in GEE.js
+++ U-Net FCNN/Calling U-net model for lithology mapping in GEE.js
@@ -1,40 +1,44 @@
-var aoi = ee.FeatureCollection("projects/ee-kaifangguo/assets/AOI");
+// JavaScript code for mapping leucogranite based on GEE
+// author: Dakai Guo,Ziye Wang
+// contact: Ziye Wang (Email: ziyewang@cug.edu.cn)
+
+// input vector file
+var aoi = ee.FeatureCollection("Asset ID");// uploaded vector file of the Himalayan orogenic belt region
 Map.centerObject(aoi)
-var cndhl = ee.FeatureCollection("projects/ee-kaifangguo/assets/cndhl");
 var BANDS = ['a3n','a04','a06','a07','s11'];
-var rsimage = ee.Image("projects/ee-kaifangguo/assets/unzhibei").select(BANDS).clip(aoi)
+var rsimage = ee.Image("Asset ID").select(BANDS).clip(aoi)// Call the pre-processed remote sensing data
 var allimage = rsimage
 print(allimage)
-//加载我们刚才训练好的模型
 
+// Calling the U-Net model endpoint in VertexAi
 var model = ee.Model.fromVertexAi({
-    endpoint:'projects/ee-kaifangguo/locations/us-central1/endpoints/2073958205938991104',
+    endpoint:'endpoints ID',
     inputTileSize:[128,128],
-    inputOverlapSize:[80,80],
-    inputShapes:{'array_image':[5]},
-    proj:ee.Projection('EPSG:4326').atScale(30),
+    inputOverlapSize:[125,125],
+    inputShapes:{'array_image':[5]}, // The length of the data string should be the size of BANDS
+    proj:ee.Projection('EPSG:4326').atScale(30), // Setting the Projection and resolution
     fixInputProj:true,
     maxPayloadBytes:5242880,
     outputBands:{
       'array': {
         'type': ee.PixelType.float(),
-        'dimensions': 1
+        'dimensions': 1 // The dimensions of the output data
       }
     }
     });
-//对图像进行预测
 
+// Lithology prediction from remote sensing data
 var predictions = model.predictImage(allimage.select(BANDS).float().toArray().rename('array_image'));
 
-
+// Exporting prediction results
 var exportParams = 
 {
   image: predictions.arrayGet([0]),
-  description: 'unet100_75',
+  description: 'unet_prediction',
   folder: 'GEE',
-  fileNamePrefix: 'unet100_75',
+  fileNamePrefix: 'unet_prediction',
   region: aoi,
-  scale: 100,
+  scale: 30,
   crs: 'EPSG:4326',
   fileFormat: 'GeoTIFF',
   maxPixels:10e10,
